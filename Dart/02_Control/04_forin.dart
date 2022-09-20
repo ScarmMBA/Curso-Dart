5b246cbe51d2bb5eb6b35b2d3d1e6068469cd365
Hiram Alejandro <71841587+ScarmMBA@users.noreply.github.com>
2022-09-20 16:50:58 -0500

    Add files via upload

---
+++ Dart/02_Control/04_forin.dart
@@ -0,0 +1,10 @@
+main(){
+  List<String> listado = ['Batman', 'Superman', 'Mujer Maravilla'];
+  /* for( int i =  0; i < listado.length; i++){
+    print(listado[i]);
+  }*/
+
+  for (String item in listado) {
+    print(item);
+  }
+}
